80786c3ce1d80ccdf5cce8ef5f2a479c1153a309
hsandinha <hebertsandinha@gmail.com>
2022-11-26 23:14:02 -0300

    .

--- js/verifica.js
+++ js/verifica.js
@@ -1,25 +1,18 @@
-
-
-async function verificaUsuario(){
+async function verificaUsuario() {
   const res = await fetch("json/usuarios.json");
   const contato = await res.json();
-  var usuarioSistema = input.usuario.value;
-  var senhaSistema = input.senha.value;
-  var encontrado = 0;
-
-      for (let i = 0; i < contato.length; i++) 
-      {
-          if(usuarioSistema == contato[i].senha && senhaSistema == contato[i].usuario){
-            href = "dados.html";
-            encontrado +=1;
-            elseif(usuarioSistema != contato[i].senha && senhaSistema == contato[i].usuario)
-              window.print("senha incorreta");
-              window.location.href = "index.html"; 
-
-            }
-      }
-      if (encontrado == 0){
-        window.print("Usuário não cadastrado");
-        window.location.href = "index.html"  
-      }            
+  usuarioSistema = document.getElementById('usuario')
+  senhaSistema = document.getElementById('senha')
+  var user = contato.find((item) => { return item.usuario == usuarioSistema.value })
+  if (user) {
+    if (user.senha == senhaSistema.value) {
+      window.location.href = "dados.html";
+    }
+    else {
+      alert("Senha invalida")
+    }
+  } 
+  else { 
+    alert("Usuário não encontrado") 
+  }
 }
